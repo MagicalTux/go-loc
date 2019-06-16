2b17a26bd18aa8a751e09037ab3adf9341775f02
Mark Karpeles <magicaltux@gmail.com>
2019-06-16 19:03:00 +0900

    add distance

--- loc.go
+++ loc.go
@@ -1,7 +1,15 @@
 package loc
 
+import "github.com/MagicalTux/go-cheapruler"
+
 type Location struct {
 	Name     string
 	Lat, Lon float64
 	Parent   *Location
+}
+
+func (l *Location) Distance(l2 *Location) float64 {
+	// compute distance with ruler
+	cr := cheapruler.New(l.Lon, cheapruler.Kilometers)
+	return cr.Distance([]float64{l.Lat, l.Lon}, []float64{l2.Lat, l2.Lon})
 }
