ea7091800b903519395aea47f907563ce6b0791c
Mark Karpeles <magicaltux@gmail.com>
2019-06-16 19:10:20 +0900

    add makeLoc

--- continent.go
+++ continent.go
@@ -1,12 +1,12 @@
 package loc
 
 var (
-	Eurasia      = &Location{Name: "Eurasia", Lat: 45.4507, Lon: 68.8319, Parent: Earth, Depth: Earth.Depth + 1}
-	Europe       = &Location{Name: "Europe", Lat: 54.5260, Lon: 15.2551, Parent: Eurasia, Depth: Eurasia.Depth + 1}
-	Asia         = &Location{Name: "Asia", Lat: 34.0479, Lon: 100.6197, Parent: Eurasia, Depth: Eurasia.Depth + 1}
-	Americas     = &Location{Name: "Americas", Lat: 54.5260, Lon: 105.2551, Parent: Earth, Depth: Earth.Depth + 1}
-	NorthAmerica = &Location{Name: "North America", Lat: 54.5260, Lon: 105.2551, Parent: Americas, Depth: Americas.Depth + 1}
-	SouthAmerica = &Location{Name: "South America", Lat: 8.7832, Lon: 55.4915, Parent: Americas, Depth: Americas.Depth + 1}
-	Australia    = &Location{Name: "Australia", Lat: 25.2744, Lon: 133.7751, Parent: Earth, Depth: Earth.Depth + 1}
-	Africa       = &Location{Name: "Africa", Lat: 8.7832, Lon: 34.5085, Parent: Earth, Depth: Earth.Depth + 1}
+	Eurasia      = makeLoc("Eurasia", 45.4507, 68.8319, Earth)
+	Europe       = makeLoc("Europe", 54.5260, 15.2551, Eurasia)
+	Asia         = makeLoc("Asia", 34.0479, 100.6197, Eurasia)
+	Americas     = makeLoc("Americas", 54.5260, -105.2551, Earth)
+	NorthAmerica = makeLoc("North America", 54.5260, -105.2551, Americas)
+	SouthAmerica = makeLoc("South America", -8.7832, -55.4915, Americas)
+	Australia    = makeLoc("Australia", 25.2744, 133.7751, Earth)
+	Africa       = makeLoc("Africa", 8.7832, 34.5085, Earth)
 )
--- loc.go
+++ loc.go
@@ -14,3 +14,13 @@ func (l *Location) Distance(l2 *Location) float64 {
 	cr := cheapruler.New(l.Lon, cheapruler.Kilometers)
 	return cr.Distance([]float64{l.Lat, l.Lon}, []float64{l2.Lat, l2.Lon})
 }
+
+func makeLoc(name string, lat, lon float64, parent *Location) *Location {
+	return &Location{
+		Name:   name,
+		Lat:    lat,
+		Lon:    lon,
+		Parent: parent,
+		Depth:  parent.Depth + 1,
+	}
+}
